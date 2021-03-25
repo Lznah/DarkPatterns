6ef31367abf5d5603b7bc328215059d1daf5d0c1
Lznah <hanzlpe@icloud.com>
2021-03-25 02:50:14 +0100

    Run.sh repaired

--- src/crawler/extract_links/run.sh
+++ src/crawler/extract_links/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # kdyz dostavas error o exception flat https://stackoverflow.com/questions/50217214/import-error-for-icu-in-mac-and-ubuntu-although-pyicu-is-installed-correctly
-celery -A celery_extract_eshop_links worker --loglevel=info -Ofair
+celery -A celery_extract_links worker --loglevel=info -Ofair
