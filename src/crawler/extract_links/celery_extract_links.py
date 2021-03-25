b002336b9bb3c12d4f77da66b7e6998e35986937
Lznah <hanzlpe@icloud.com>
2021-03-25 01:50:05 +0100

    Exract links crawler

---
+++ src/crawler/extract_links/celery_extract_links.py
@@ -0,0 +1,50 @@
+import sys
+import celery
+import csv
+from pyvirtualdisplay import Display
+from celery.exceptions import TimeLimitExceeded, MaxRetriesExceededError
+from logger import logger
+from crawler import crawl, VIRT_DISPLAY_DIMS
+from time import sleep
+
+def get_host():
+    import os
+    host = os.environ.get('AMQP_HOST')
+    if host is None:
+        host = "127.0.0.1"
+    return host
+
+BROKER_NETWORK="pyamqp://guest@%s//" % get_host()
+MAX_RETRIES = 3
+MAX_SPIDERING_DURATION = 30*60
+HARD_TIMEOUT = MAX_SPIDERING_DURATION+30
+
+app = celery.Celery('celery_extract_eshop_links', broker=BROKER_NETWORK)
+
+@app.task(name="celery_extract_eshop_links", max_retries=MAX_RETRIES, time_limit=HARD_TIMEOUT, ignore_result=True)
+def call_crawl(page):
+    try:
+        display = Display(visible=True, size=VIRT_DISPLAY_DIMS)
+        display.start()
+        crawl(page)
+    except MaxRetriesExceededError:
+        logger.error("MaxRetriesExceededError while crawling page %s" % page)
+    except TimeLimitExceeded:
+        logger.error("TimeLimitExceeded while crawling page %s" % page)
+    except Exception:
+        logger.exception("Exception while crawling page %s" % page)
+    finally:
+        if display:
+            display.stop()
+
+def main(domains_file):
+    with open(domains_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            url_address = row[3]
+            call_crawl.delay(url_address)
+            break
+        logger.info("Finished adding %d URLs to celery queue" % len(csv_reader))
+
+if __name__ == '__main__':
+    main(sys.argv[1])
