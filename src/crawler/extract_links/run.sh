aa76ae7eb94892086e530149140e00751fdea13c
Lznah <hanzlpe@icloud.com>
2021-04-13 07:06:52 +0200

    Edited run.sh and create script to concat gathered product links

--- src/crawler/extract_links/run.sh
+++ src/crawler/extract_links/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+# if you get an exception flat error, follow steps on https://stackoverflow.com/questions/50217214/import-error-for-icu-in-mac-and-ubuntu-although-pyicu-is-installed-correctly
 
-# kdyz dostavas error o exception flat https://stackoverflow.com/questions/50217214/import-error-for-icu-in-mac-and-ubuntu-although-pyicu-is-installed-correctly
-celery -A celery_extract_links worker --loglevel=info -Ofair
+celery -A celery_extract_links worker --loglevel=info -Ofair --detach
