9378f1b2509d0e0180a692323b10cd863f5783d4
Lznah <hanzlpe@icloud.com>
2021-06-14 04:23:48 +0200

    Added script for cherrypicking domans

---
+++ src/crawler/extract_links/cherrypick_first_number_shops.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df1 = pd.read_csv('../reveal_true_domains/clean_eshop_list_manually_edited.csv', delimiter=",", header=None, encoding='utf-8')
+
+links=[]
+with open("concatenated_product_links.csv","r") as r:
+    links=r.readlines()
+links = [x.replace("\n","") for x in links]
+
+#get first 10000 rows
+domains = df1[3].head(10000).to_list()
+print(len(links), len(domains))
+
+for i in links:
+    if any(i for j in domains if str(j) in i):
+        print(i)
