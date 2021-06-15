cef0e6de4cf36df1eeddf7bf4c9c9bdc31fa466b
Lznah <hanzlpe@icloud.com>
2021-06-15 04:09:12 +0200

    Another minor changes

--- src/crawler/extract_textual_segments/run.sh
+++ src/crawler/extract_textual_segments/run.sh
@@ -2,8 +2,8 @@
 # this script creates a nohup process that keeps xvfb alive 
 # to change number of parallel browsers, open segment_pilot_crawl.py and edit NUM_BROWSERS variable
 
-nohup ./keep_xvfb_alive.sh > nohup.log 2>&1 &
-echo $! > save_pid.txt
+./keep_xvfb_alive.sh &
 
-DISPLAY=:99 nohup python segment_pilot_crawl.py $1 &
-echo $! >> save_pid.txt
+DISPLAY=:99 
+
+python segment_pilot_crawl.py $1
