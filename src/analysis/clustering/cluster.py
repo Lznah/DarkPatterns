db219e57232dca71db593a3d190bf2e5e4a2ec60
Lznah <hanzlpe@icloud.com>
2021-06-17 20:49:00 +0200

    Support python3

--- src/analysis/clustering/cluster.py
+++ src/analysis/clustering/cluster.py
@@ -4,17 +4,17 @@ import hdbscan
 import pandas as pd
 
 if (len(sys.argv) != 5):
-    print 'Usage: python cluster.py [feature_file] [min_cluster_size] [metric] [label_pickle_file]'
+    print('Usage: python cluster.py [feature_file] [min_cluster_size] [metric] [label_pickle_file]')
     sys.exit(1)
 
 arr = pd.read_csv(sys.argv[1], sep=' ', header=None, dtype=np.float64).values
-print arr.shape
+print(arr.shape)
 
 clusterer = hdbscan.HDBSCAN(min_cluster_size=int(sys.argv[2]), metric=sys.argv[3], core_dist_n_jobs=-1, algorithm='boruvka_kdtree')
 cluster_labels = clusterer.fit_predict(arr)
 ser = pd.Series(cluster_labels)
 
-print ser.value_counts().to_string()
+print(ser.value_counts().to_string())
 
 ser.to_pickle(sys.argv[4])
 
